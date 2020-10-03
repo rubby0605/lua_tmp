28ed92e29d0bf806625eb63a6d5276c43818a3a2
Lin Tu <8394443+rubby0605@users.noreply.github.com>
2020-10-03 21:26:33 +0800

    Update readme.sh

--- P4_fit_by_c/fit/readme.sh
+++ P4_fit_by_c/fit/readme.sh
@@ -1,7 +1,7 @@
 rm *.o
 gcc -c -fPIC linear.c -o linear.o -lm -llapack -lblas
 gcc -c -fPIC main.c -o main.o -lm -llapack -lblas
-gcc *.o -L/home/qq/.local/ -shared -o liblinreg.so -lm -llapack -lblas
+gcc *.o -L/PATH_TO_LIB -shared -o liblinreg.so -lm -llapack -lblas
 #gcc -Wall -std=c99 -c  band_solve.c -I/usr/include -o band_solve.o -lm -llapacke -lblas;
 #gcc -Wall -std=c99 -Wfatal-errors -Werror -c  itrCalcT.c -I/usr/include -o itrCalcT.o -lm -llapacke -lblas;
 #gcc *.o -L/home/qq/.local/ -llapack -lblas -lm -o code
